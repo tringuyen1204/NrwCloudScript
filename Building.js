a37a3607837a28a07c40c30d9df1664084a70e2b
Tri Nguyen Ngo Minh <tringuyen1204@gmail.com>
2017-08-11 09:26:02 +0700

    - delete old building logic

--- Building.js
+++ Building.js
@@ -1,19 +1,1 @@
-handlers.UpgradeBuilding = function(args) {
 
-  // get the calling player's inventory and VC balances
-	var GetUserInventoryRequest = {
-        "PlayFabId": currentPlayerId
-    };
-
-  var GetUserInventoryResult = server.GetUserInventory(GetUserInventoryRequest);
-}
-
-class BuildingBuilder{
-
-  constructor(buildingId){
-    this.id = buildingId;
-  }
-
-  function upgrade(){
-  }
-}
