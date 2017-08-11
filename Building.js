f978ff3e703dcb9585d87c708a8c406048bcbce4
Tri Nguyen Ngo Minh <tringuyen1204@gmail.com>
2017-08-11 13:31:16 +0700

    - upgrade building

--- Building.js
+++ Building.js
@@ -1,15 +1,46 @@
-function GetBuidingData(buildingID){
+function GetBuidingData(buildingType){
   var GetCatalogItemsRequest = {
-    "CatalogVersion ": buildingID
+    "CatalogVersion ": buildingType
   };
 
   var GetCatalogItemsResults = server.GetCatalogItems(GetCatalogItemsRequest);
   return GetCatalogItemsResults;
 }
 
+function ConstructBuilding(buildingType, index){
+
+  var buildingId = buildingType + index;
+
+  var GetUserDataRequest = {
+    "PlayFabId":currentPlayerId,
+    "Keys": [buildingId]
+  }
+
+  var GetUserDataResult = server.GetUserData(GetUserDataRequest);
+
+  var curLv = 0
+  if (GetUserDataResult.Data[buildingId] != null){
+    curLv = GetUserDataResult.Data[buildingId]["lvl"];
+  }
+  curLv += 1;
+
+  var UpdateUserDataRequest = {
+    "PlayFabId":currentPlayerId,
+    "Data":{
+      buildingId:{
+        "lvl":curLv,
+        "updateTime":server.GetServerTime()
+      }
+    }
+  };
+
+  server.UpdateUserData(UpdateUserDataRequest);
+}
 
 handlers.Test = function(args){
-  var result = GetBuidingData(args.buildingID);
-  log.debug("data: ", result.data);
+  //var result = GetBuidingData(args.buildingType);
+  //log.debug("data: ", result.data);
+
+  ConstructBuilding("castle", 0);
   return 1;
 }
