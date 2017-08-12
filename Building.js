7db64db5cafdf7c47222e3178010048aa68323dd
Tri Nguyen Ngo Minh <tringuyen1204@gmail.com>
2017-08-12 10:29:53 +0700

    no message

--- Building.js
+++ Building.js
@@ -18,13 +18,17 @@ function ConstructBuilding(buildingType, index){
   var GetUserDataResult = server.GetUserReadOnlyData(GetUserReadOnlyDataRequest);
 
   var curLv = 0;
+  var allBuildings;
   if (GetUserDataResult.Data[buildingType] != null){
-    var allBuildings = JSON.parse(GetUserDataResult.Data[buildingType]);
-
+    allBuildings = JSON.parse(GetUserDataResult.Data[buildingType]);
     if (allBuildings.length > index){
       curLv = allBuildings[index].lvl;
     }
   }
+  else {
+    allBuildings = {};
+  }
+
   curLv += 1;
 
   var newBuildingData = {
