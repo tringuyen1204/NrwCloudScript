e9366939aa2a7294853c088533ea1ac578a54856
Tri Nguyen Ngo Minh <tringuyen1204@gmail.com>
2017-09-06 15:16:25 +0700

    - Barrack

--- Constant.js
+++ Constant.js
@@ -23,6 +23,6 @@ var SKR = "Skirmisher";
 var CAV = "Cavalry";
 
 // time
-ONE_MINUTE = 60000;
-ONE_HOUR = 3600000;
-ONE_DAY = 86400000;
+ONE_MINUTE = 60 * 1000;
+ONE_HOUR = 3600 * 1000;
+ONE_DAY = 86400 * 1000;
