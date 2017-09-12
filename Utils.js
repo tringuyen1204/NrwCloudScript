97cffc6e39cfffdf2eb37d5ed1d4b0890b75ba02
Tri Nguyen Ngo Minh <tringuyen1204@gmail.com>
2017-09-12 11:13:54 +0700

    - Barrack2

--- Utils.js
+++ Utils.js
@@ -1,30 +1,32 @@
+var Converter = {};
+
 /**
  * @return(diamondCost)
  */
-function ConvertTimeToDiamond(milisec) {
+Converter.TimeToDiamond = function (milisec) {
     if (milisec <= 0)
-    return 0;
+        return 0;
 
     var hours = milisec / ONE_HOUR;
-  var ret = 25.18375 * Math.pow(hours, 0.7513);
+    var ret = 25.18375 * Math.pow(hours, 0.7513);
 
-  if (ret > Math.floor(ret))
-    ret = ret + 1;
+    if (ret > Math.floor(ret))
+        ret = ret + 1;
 
-  return ret;
-}
+    return ret;
+};
 
 /**
  * @return(diamondCost)
  */
-function ConvertGoldFoodToDiamond(qty) {
+Converter.GoldFoodToDiamond = function (qty) {
     if (qty <= 0)
         return 0;
     var ret = Math.pow(6, Math.log(qty / 100) / Math.log(10));
     if (ret < 1)
         ret = 1;
     return Math.floor(ret);
-}
+};
 
 /**
  * @return(boolean)
@@ -46,24 +48,23 @@ function SpendCurrency(code, qty){
 function ChangeCurrency(vcBalances, code, qty)
 {
     if (vcBalances !== null
-    && vcBalances.hasOwnProperty(code)
-    && (vcBalances[code] + qty >= 0) )
-	{
-		vcBalances[code] += qty;
-	}
+        && vcBalances.hasOwnProperty(code)
+        && (vcBalances[code] + qty >= 0) ) {
+        vcBalances[code] += qty;
+    }
 
-  if (qty > 0){
-    server.AddUserVirtualCurrency({
-      "PlayFabId" : currentPlayerId,
-      "VirtualCurrency": code,
-      "Amount": Math.abs(qty)
-    });
-  }
-  else {
-    server.SubtractUserVirtualCurrency({
-      "PlayFabId" : currentPlayerId,
-      "VirtualCurrency": code,
-      "Amount": Math.abs(qty)
-    });
-  }
+    if (qty > 0){
+        server.AddUserVirtualCurrency({
+            "PlayFabId" : currentPlayerId,
+            "VirtualCurrency": code,
+            "Amount": Math.abs(qty)
+        });
+    }
+    else {
+        server.SubtractUserVirtualCurrency({
+            "PlayFabId" : currentPlayerId,
+            "VirtualCurrency": code,
+            "Amount": Math.abs(qty)
+        });
+    }
 }
